7c24d2dde8175785b64e7d0226033f25bc313183
atihiphob <94682067+atihiphob@users.noreply.github.com>
2022-02-14 22:40:43 +0300

    Add files via upload

--- PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/Main.java
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/Main.java
@@ -1,22 +1,18 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.SQLException;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoJDBCImpl tablet = new UserDaoJDBCImpl();
-        tablet.createUsersTable();
+        UserDaoHibernateImpl tablet = new UserDaoHibernateImpl();
         tablet.saveUser("Вася", "Пупкин", (byte) 2);
         tablet.saveUser("Вася", "Пупкин", (byte) 2);
         tablet.saveUser("Вася", "Пупкин", (byte) 2);
         tablet.saveUser("Вася", "Пупкин", (byte) 2);
-        List <User> userList = tablet.getAllUsers();
-        System.out.println(userList);
+        List <User> userList1 = tablet.getAllUsers();
+        System.out.println(userList1);
         tablet.cleanUsersTable();
         tablet.dropUsersTable();
 
--- PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/model/User.java
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,21 +1,22 @@
 package jm.task.core.jdbc.model;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-@Table
+@Entity
+@Table (name = "users")
 public class User {
     @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
     private Long id;
 
-    @Column
+    @Column (name = "name")
     private String name;
 
-    @Column
+    @Column (name = "lastName")
     private String lastName;
 
-    @Column
+    @Column (name = "age")
     private Byte age;
 
     public User() {
@@ -61,6 +62,6 @@ public class User {
     }
 
     public String toString () {
-        return "[id = " + id + ", name = " + name + ", lastName = " + lastName + ", age = " + age + "]";
+        return " user [id = " + id + ", name = " + name + ", lastName = " + lastName + ", age = " + age + "]";
     }
 }
